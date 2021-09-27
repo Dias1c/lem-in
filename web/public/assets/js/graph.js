16e38c308500f18512639cb9470587a6c82ec343
Dias1c <52798029+Dias1c@users.noreply.github.com>
2021-09-27 21:41:06 +0600

    visualization and html Styles finished

---
+++ web/public/assets/js/graph.js
@@ -0,0 +1,429 @@
+// import { d3 } from "https://d3js.org/d3.v7.min.js";
+// import * as d3 from "d3";
+class Graphs {
+    static PatternNodeName = `[.\\dA-KM-Za-zА-Яа-я]{1}[.\\d\\wА-Яа-я]{0,}`
+    static Palette = { Default: "#1995AD", Start: "#F52549", Finish: "#6af030", Unusing: "#BCBABE", Using: "#1E434C", }
+
+    constructor(content) {
+        this.Reconsctruct(content)
+    }
+    /* == PUBLIC == */
+    // Reset All Values By Content
+    Reconsctruct(content) {
+        this.ClearValues();
+        if (!content) {
+            return
+            // throw { message: "Graph.Reconstructor 'content' undefined or null" }
+        }
+        let lines = content.split(/\r?\n/)
+        // console.log(`Graph Constructor:\nContent = "${content}"\nLines = [${lines}]`)
+        lines = this.getNodesWithoutCountAnts(lines)
+        // console.log(`After getNodesWithoutCountAnts: ${lines}`)
+        lines = this.setNodes(lines)
+        lines = this.setLinks(lines)
+        if (this.Start != "") {
+            this.Steps.push([this.Start])
+        }
+        lines = this.setSteps(lines)
+        this.SetColors()
+    }
+    // Clear
+    ClearValues() {
+        this.Start = ""
+        this.Finish = ""
+        this.Nodes = {}
+        this.NodeNames = []
+        this.Links = []
+        this.Steps = []
+        this.Paths = []
+    }
+
+    SetColors() {
+        // Set Default Colors
+        this.NodeNames.forEach((name) => {
+            this.Nodes[name].Color = {
+                Default: Graphs.Palette.Unusing, Active: Graphs.Palette.Unusing
+            }
+        });
+        //Set Colors for Actice
+        this.Paths.forEach((path) => {
+            let r = getRandom(255),
+                g = getRandom(255),
+                b = getRandom(255);
+            let colorDefault = getRgba(r, g, b, 0.5),
+                colorActive = getRgba(r, g, b, 1);
+            path.forEach((name) => {
+                this.Nodes[name].Color = { Default: colorDefault, Active: colorActive }
+            })
+        })
+
+        if (this.NodeNames.length != 0) {
+            this.Nodes[this.Start].Color = { Default: Graphs.Palette.Start, Active: Graphs.Palette.Start }
+            this.Nodes[this.Finish].Color = { Default: Graphs.Palette.Finish, Active: Graphs.Palette.Finish }
+        }
+    }
+
+    /* == PRIVATE == */
+    getNodesWithoutCountAnts(lines) {
+        if (lines == null) {
+            throw { message: "lines is null, (setNodes)" }
+        } else if (lines.length < 1) {
+            throw { message: "lines.length < 1, (setNodes)" }
+        }
+        let startIdx = 0;
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i]
+            startIdx = i + 1
+            if (line.startsWith("#") || line == "") { // Check for comment
+                continue
+            } else {
+                let countAnts = parseInt(line);
+                if (Number.isInteger(countAnts)) {
+                    this.CountAnts = countAnts
+                    break
+                } else {
+                    startIdx--
+                }
+            }
+        }
+        return lines.slice(startIdx)
+    }
+    setNodes(lines) {
+        if (lines == null) {
+            throw { message: "lines is null, (setNodes)" }
+        } else if (lines.length < 2) {
+            throw { message: "lines.length < 2, (setNodes)" }
+        }
+        let startIdx = 0;
+
+        // Gets All Rooms
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i]
+            startIdx = i + 1
+            if (line.startsWith("#")) { // Check for comment or Start|End Rooms
+                let isStart = false
+                if (line == "##start") {
+                    isStart = true
+                }
+                // check for start or end room
+                if (line == "##start" || line == "##end") {
+                    i++
+                    startIdx++
+                    if (i < lines.length) {
+                        line = lines[i]
+                        let node = this.addNode(line)
+                        if (node == null) {
+                            throw { message: "after ##start or ##end should be node" }
+                        }
+                        //Get Room
+                        if (node == null) {
+                            throw { message: "invalid rooms for start or end not found" }
+                        } else if (isStart && this.Start == "") {
+                            this.Start = node.Name
+                        } else if (!isStart && this.Finish == "") {
+                            this.Finish = node.Name
+                        } else {
+                            throw { message: "there can be only 1 starting and 1 ending rooms on the anthill" }
+                        }
+                    } else {
+                        throw { message: "invalid rooms for start or end not found" }
+                    }
+                } else {
+                    continue
+                }
+            } else if (line == "") {
+                continue
+            } else {
+                // Take rooms or break if is not valid room
+                let node = this.addNode(line)
+                if (node == null) {
+                    startIdx = startIdx - 1
+                    break
+                }
+            }
+        }
+        if (this.Start == "" || this.Finish == "") {
+            throw { message: "invalid rooms for start or end not found." }
+        }
+        return lines.slice(startIdx)
+    }
+    setLinks(lines) {
+        if (lines == null) {
+            throw { message: "lines is null, (setLinks)" }
+        } else if (lines.length < 1) {
+            throw { message: "lines.length < 2 (No Nodes), (setLinks)" }
+        }
+        let startIdx = 0;
+        // Gets All Rooms
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i]
+            startIdx = i + 1
+            if (line.startsWith("#") || line == "") { // Check for comment 
+                continue
+            } else {
+                let link = this.addLink(line)
+                if (link == null) {
+                    startIdx--
+                    break
+                } else if (!this.Nodes[link.Source] || !this.Nodes[link.Target]) {
+                    throw { message: `Undifined room Name: "${link.Source}" or "${link.Target}"` }
+                }
+            }
+        }
+        return lines.slice(startIdx)
+    }
+    setSteps(lines) {
+        if (lines == null) {
+            throw { message: "lines is null, (setSteps)" }
+        }
+        let steps = []
+        let startIdx = 0;
+        // Gets All Steps
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i]
+            startIdx = i + 1
+            if (line.startsWith("#") || line == "") { // Check for comment 
+                continue
+            } else {
+                let step = this.addStep(line)
+                if (step == null) {
+                    startIdx--
+                    break
+                }
+                steps.push(step)
+            }
+        }
+
+        let paths = []
+        let ants = {}
+        for (let i = 0; i < steps.length; i++) {
+            if (i == 0) {
+                steps[i].forEach((element, index) => {
+                    ants[element.Ant] = { index: index }
+                    paths.push([])
+                })
+            }
+            let isBreak = false
+            steps[i].forEach((element) => {
+                if (ants[element.Ant]) {
+                    paths[ants[element.Ant].index].push(element.NodeName)
+                }
+            })
+            if (isBreak) {
+                break
+            }
+        }
+        this.Paths = paths
+        return lines.slice(startIdx)
+    }
+
+    // For Add Node, Gets Node {Name, X, Y}
+    addNode(line) {
+        if (this.Nodes == null) {
+            this.Nodes = {}
+        }
+        let regex = new RegExp(`^(${Graphs.PatternNodeName}) (\\d{1,}) (\\d{1,})$`, 'gm')
+        let match, node = {};
+        if (match = regex.exec(line)) {
+            node.Name = match[1],
+                node.X = parseInt(match[2]),
+                node.Y = parseInt(match[3]);
+        } else {
+            return null
+        }
+        if (node != null && node.Name == "") {
+            throw { message: "incorrect node: bad name", line }
+        }
+        this.Nodes[node.Name] = node
+        this.NodeNames.push(node.Name)
+        return node
+    }
+    // For Add Link, Gets Link {Sourse, Target}
+    addLink(line) {
+        if (this.Links == null) {
+            this.Links = []
+        }
+        let regex = new RegExp(`^(${Graphs.PatternNodeName})-(${Graphs.PatternNodeName})$`, 'gm')
+        let match, result = {};
+        if (match = regex.exec(line)) {
+            result.Source = match[1];
+            result.Target = match[2];
+        } else {
+            return null
+        }
+        this.Links.push(result)
+        return result
+    }
+    // For Add Step, Gets Current Ant Step info {Ant, Node}
+    addStep(line) {
+        if (this.Steps == null) {
+            this.Steps = null
+        }
+        let regex = new RegExp(`L(\\d{1,})-(${Graphs.PatternNodeName})`, 'gm')
+        const match = [...line.matchAll(regex)];
+        if (match == null) {
+            return null
+        }
+        let roomNames = []
+        let result = []
+        match.forEach(element => {
+            roomNames.push(element[2])
+
+            result.push({ Ant: element[1], NodeName: element[2] })
+        });
+        this.Steps.push(roomNames)
+        return result
+    }
+}
+
+class GraphDrawer {
+    Palette = { Default: "#1995AD", Start: "#F52549", Finish: "#6af030", Unusing: "#BCBABE", Using: "#1E434C", } // For Configure
+    TileSize = 20
+    CircleRadius = 10
+    LineWidth = 4
+    Transition = 500
+    GraphCircles = {}
+
+    // Graph
+    Graph = {}//new Graphs() // {}
+    // D3 Element
+    html_svg = {}
+    html_g = {}
+    // 
+    Circles = {}
+
+    constructor(blockId, graph) {
+        this.Graph = graph
+        // this.ConfigureGraph() // if colors value to graph not setted
+        this.InitHtmlElements(blockId)
+    }
+
+    /* == PUBLIC == */
+    InitHtmlElements(blockId) {
+        // Clear svg Element in Block
+        let html_block = d3.select(blockId)
+        html_block.select("svg").remove()
+        // Create Elements (INIT)
+        this.html_svg = html_block
+            .append("svg")
+            .attr("viewBox", [0, 0, 500, 500])
+            .attr("cursor", "grab");;
+        let eventZoom = ({ transform }) => {
+            this.html_g.attr("transform", transform);
+        }
+        this.html_svg
+            .call(d3.zoom()
+                .on("zoom", eventZoom
+                ));
+        this.html_g = this.html_svg.append("g")
+    }
+    ConfigureGraph() {
+        // Set Default Colors
+        this.Graph.NodeNames.forEach((name) => {
+            this.Graph.Nodes[name].Color = {
+                Default: this.Palette.Unusing, Active: this.Palette.Unusing
+            }
+        });
+        //Set Colors for Actice
+        this.Graph.Paths.forEach((path) => {
+            let r = getRandom(255),
+                g = getRandom(255),
+                b = getRandom(255);
+            let colorDefault = getRgba(r, g, b, 0.5),
+                colorActive = getRgba(r, g, b, 1);
+            path.forEach((name) => {
+                this.Graph.Nodes[name].Color = { Default: colorDefault, Active: colorActive }
+            })
+        })
+        if (this.Graph.NodeNames.length != 0) {
+            this.Graph.Nodes[this.Graph.Start].Color = { Default: this.Palette.Start, Active: this.Palette.Start }
+            this.Graph.Nodes[this.Graph.Finish].Color = { Default: this.Palette.Finish, Active: this.Palette.Finish }
+        }
+    }
+    ReDrawGraph() {
+        this.html_g.html("");
+        this.AddGraphToGroup();
+    }
+    AddGraphToGroup() {
+        this.addLines();
+        this.addCircles();
+    }
+    ShowFrame(frame) {
+        if (0 <= frame && frame < this.Graph.Steps.length) {
+
+            let usedNodes = {}
+            this.Graph.Steps[frame].forEach((name) => {
+                this.Circles[name]
+                    .transition()
+                    .duration(this.Transition)
+                    .attr("r", this.CircleRadius + (this.CircleRadius * 0.2))
+                    .style("fill", this.Graph.Nodes[name].Color.Active)
+                usedNodes[name] = true
+            })
+
+            this.Graph.NodeNames.forEach((name) => {
+                if (!usedNodes[name]) {
+                    this.Circles[name].transition()
+                        .duration(this.Transition)
+                        .attr("r", this.CircleRadius)
+                        .style("fill", this.Graph.Nodes[name].Color.Default)
+                }
+            })
+        }
+    }
+    /* == PRIVATE == */
+    addLines() {
+        this.Graph.Links.forEach((node) => {
+            let from = this.Graph.Nodes[node.Source]
+            let to = this.Graph.Nodes[node.Target]
+
+            this.html_g
+                .append("line")
+                .style("stroke-linecap", "round")
+                .style("stroke-width", this.LineWidth)
+                .attr("x1", from.X * this.TileSize).attr("y1", from.Y * this.TileSize)
+                .attr("x2", to.X * this.TileSize).attr("y2", to.Y * this.TileSize);
+        })
+
+    }
+    addCircles() {
+        // let node = "nodeName"
+        this.Graph.NodeNames.forEach((name) => {
+            let node = this.Graph.Nodes[name]
+            let posX = node.X * this.TileSize
+            let posY = node.Y * this.TileSize
+
+            this.Circles[name] = this.html_g
+                .append("circle")
+                .attr("cx", posX)
+                .attr("cy", posY)
+                .attr("r", this.CircleRadius)
+                .style("fill", node.Color.Default)
+            this.html_g
+                .append("text")
+                .attr("x", posX)
+                .attr("y", posY - (this.CircleRadius + 5))
+                .attr("fill", "#000")
+                .text(`${node.Name}`)
+                .attr("text-anchor", "middle")
+        });
+        if (this.Circles[this.Graph.Start]) {
+            this.Circles[this.Graph.Start].style("stroke", "#000")
+        }
+        if (this.Circles[this.Graph.Finish]) {
+            this.Circles[this.Graph.Finish].style("stroke", "#000")
+        }
+    }
+}
+
+
+
+// Utils
+function getRandom(max) {
+    let o = Math.round, r = Math.random;
+    return o(r() * max)
+}
+
+function getRgba(r, g, b, opacity) {
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
--- web/public/assets/js/index.js
+++ web/public/assets/js/index.js
@@ -1,6 +1,14 @@
+// To Do Frame Log on HTML
+
 var btn_SendContent = document.getElementById("btn_SendContent")
+var btn_SetParams = document.getElementById("btn_SetParams")
+var btn_Animate = document.getElementById("btn_Animate")
+var tb_FrameIndex = document.getElementById("tb_FrameIndex")
+var l_FrameInfo = document.getElementById("l_FrameInfo")
 var tb_Content = document.getElementById("tb_Content")
 var tb_Result = document.getElementById("tb_Result")
+var IsFramePlay = false
+
 var popup = {
     Window: document.getElementById("window"),
     Title: document.getElementById("windowTitle"),
@@ -9,12 +17,16 @@ var popup = {
     Btn_Close: document.getElementById("windowClose")
 }
 
+let Drawwer = new GraphDrawer("#b_result", new Graphs(document.getElementById("tb_Result").value))
+
+
 window.addEventListener("load", OnLoad)
 
 // Events
 function OnLoad() {
-    btn_SendContent.addEventListener("click", btn_SendContent_OnClick)
+    InitEvents()
     InitPopup()
+    SetDefaultValuesOnHtml()
 }
 
 async function btn_SendContent_OnClick() {
@@ -39,11 +51,74 @@ async function btn_SendContent_OnClick() {
         tb_Result.value = await response.text();//(await response.text()).toString().split('\\n').join("\n")
         // Start Show Graph
         btn_SendContent.disabled = false
+        btn_SetParams_OnClick()
     } catch (error) {
         //Show Pop Up window
         let enableBtn = () => { btn_SendContent.disabled = false }
         ShowPopupWindow("Error", error.message, enableBtn, enableBtn)
     }
+}
+function btn_SetParams_OnClick() {
+    try {
+        let transition = parseInt(d3.select("#tb_Transition").node().value)
+        let tileSize = parseInt(d3.select("#tb_TileSize").node().value)
+        let circleRadius = parseInt(d3.select("#tb_CircleRadius").node().value)
+        if (Number.isNaN(transition) || transition < 4) {
+            throw { message: "Parametr: Transition < 4ms\nParametrs Not Accepted!" }
+        } else if (Number.isNaN(tileSize) || tileSize < 1) {
+            throw { message: "Parametr: Tile Size < 1px\nParametrs Not Accepted!" }
+        } else if (Number.isNaN(circleRadius) || circleRadius < 1) {
+            throw { message: "Parametr: Circle Radius < 1px\nParametrs Not Accepted!" }
+        }
+        Drawwer.TileSize = tileSize
+        Drawwer.CircleRadius = parseInt(d3.select("#tb_CircleRadius").node().value)
+        Drawwer.LineWidth = Drawwer.CircleRadius / 2
+        Drawwer.Transition = transition
+
+        Drawwer.Graph = new Graphs(document.getElementById("tb_Result").value)
+        Drawwer.ReDrawGraph()
+        ResetFrameValues()
+    } catch (error) {
+        ShowPopupWindow("Error", error.message)
+    }
+}
+function btn_Animate_OnClick() {
+    IsFramePlay = true
+    EnambleAnimationControllers(IsFramePlay)
+    function animate(frame) {
+        let min = parseInt(tb_FrameIndex.min)
+        let max = parseInt(tb_FrameIndex.max)
+
+        Drawwer.ShowFrame(frame)
+        // l_FrameInfo.innerHTML = `${tb_FrameIndex.value}/${tb_FrameIndex.max}`
+        if (frame <= min) {
+            frame = min
+        } else if (frame >= max) {
+            tb_FrameIndex.value = frame
+            frame = max
+            IsFramePlay = false
+        }
+        tb_FrameIndex.value = frame
+        l_FrameInfo.innerHTML = `${tb_FrameIndex.value}/${tb_FrameIndex.max}`
+        if (IsFramePlay) {
+            setTimeout(() => { animate(frame + 1) }, Drawwer.Transition)
+        } else {
+            EnambleAnimationControllers(IsFramePlay)
+        }
+    }
+    animate(0)
+}
+
+
+// FUNCTIONS
+function InitEvents() {
+    btn_SendContent.addEventListener("click", btn_SendContent_OnClick)
+    btn_SetParams.addEventListener("click", btn_SetParams_OnClick)
+    btn_Animate.addEventListener("click", btn_Animate_OnClick)
+    tb_FrameIndex.addEventListener("input", () => {
+        Drawwer.ShowFrame(parseInt(tb_FrameIndex.value))
+        l_FrameInfo.innerHTML = `${tb_FrameIndex.value}/${tb_FrameIndex.max}`
+    })
 }
 
 function InitPopup() {
@@ -52,19 +127,106 @@ function InitPopup() {
 }
 
 function ShowPopupWindow(title, message, onClickOk, onClickClose) {
+    // Do Old Window Event
+    if (popup.Window.style.display == "block") {
+        popup.Btn_Close.click()
+    }
+    // Set Values
     if (onClickOk != null) {
         popup.Btn_Ok.addEventListener("click", () => {
             popup.Window.style.display = "none"
             onClickOk()
         })
+    } else {
+        popup.Btn_Ok.addEventListener("click", () => {
+            popup.Window.style.display = "none"
+        });
     }
     if (onClickClose != null) {
         popup.Btn_Close.addEventListener("click", () => {
             popup.Window.style.display = "none"
             onClickClose()
         })
+    } else {
+        popup.Btn_Close.addEventListener("click", () => {
+            popup.Window.style.display = "none"
+        });
     }
     popup.Title.textContent = title
     popup.Text.textContent = message
     popup.Window.style.display = "block"
+}
+
+function SetDefaultValuesOnHtml() {
+    tb_Result.value = `20
+a 0 0
+##start
+b 0 4
+c 2 2
+d 4 0
+e 3 4
+f 6 2
+g 6 4
+h 5 7
+i 9 0
+j 9 4
+k 9 6
+l 7 8
+##end
+m 12 4
+n 12 8
+a-b
+b-e
+e-g
+g-j
+j-m
+a-d
+d-i
+i-j
+b-c
+c-f
+f-g
+e-h
+h-l
+l-n
+n-m
+g-k
+k-m
+
+L1-a L2-e L3-c 
+L1-d L2-h L3-f L4-a L5-e L6-c 
+L1-i L2-l L3-g L4-d L5-h L6-f L7-a L8-e L9-c 
+L1-j L2-n L3-k L4-i L5-l L6-g L7-d L8-h L9-f L10-a L11-e L12-c 
+L1-m L2-m L3-m L4-j L5-n L6-k L7-i L8-l L9-g L10-d L11-h L12-f L13-a L14-e L15-c 
+L4-m L5-m L6-m L7-j L8-n L9-k L10-i L11-l L12-g L13-d L14-h L15-f L16-a L17-e L18-c 
+L7-m L8-m L9-m L10-j L11-n L12-k L13-i L14-l L15-g L16-d L17-h L18-f L19-a L20-e 
+L10-m L11-m L12-m L13-j L14-n L15-k L16-i L17-l L18-g L19-d L20-h 
+L13-m L14-m L15-m L16-j L17-n L18-k L19-i L20-l 
+L16-m L17-m L18-m L19-j L20-n 
+L19-m L20-m 
+`
+    btn_SetParams_OnClick()
+    ResetFrameValues()
+}
+
+function EnambleAnimationControllers(enable) {
+    btn_Animate.disabled = enable
+    tb_FrameIndex.disabled = enable
+}
+
+function ResetFrameValues() {
+    let min = 0,
+        value = tb_FrameIndex.value,
+        max = Drawwer.Graph.Steps.length - 1;
+    if (value > max || value < min) {
+        value = min
+        console.log("Value changed")
+    }
+
+    tb_FrameIndex.min = min
+    tb_FrameIndex.value = value
+    tb_FrameIndex.max = max
+    l_FrameInfo.innerHTML = `${value}/${max}`
+    Drawwer.ShowFrame(value)
+    IsFramePlay = false
 }
